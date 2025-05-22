d50766f8041a2a7b2f853866f4734b3f51dc58e3
chaenii25 <cheun9325@naver.com>
2025-05-22 14:27:10 +0900

    update Controller

--- src/main/java/com/example/travel/controller/TravelController.java
+++ src/main/java/com/example/travel/controller/TravelController.java
@@ -4,9 +4,10 @@ import com.example.travel.dto.TravelDTO;
 import com.example.travel.service.TravelService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/api/list")
 public class TravelController {
 
@@ -18,6 +19,7 @@ public class TravelController {
 
     //csv파일을 DB에 저장
     @PostMapping("/load")
+    @ResponseBody
     public String loadTravel(@RequestParam String path) {
         travelService.loadTravelFromCSV(path);
         return "CSV 업로드 및 저장 완료";
@@ -25,6 +27,7 @@ public class TravelController {
 
     //관광지 전체 목록 조회
     @GetMapping
+    @ResponseBody
     public Page<TravelDTO> getAllTravels(@RequestParam(defaultValue = "0") int page,
                                          @RequestParam(defaultValue = "20") int size) {
         return travelService.getTravels(PageRequest.of(page, size));
