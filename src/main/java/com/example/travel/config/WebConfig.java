0625e83829b316cca859984416bcd0de9c6af9e8
chaenii25 <cheun9325@naver.com>
2025-05-22 14:18:35 +0900

    update WebConfig.java

--- src/main/java/com/example/travel/config/WebConfig.java
+++ src/main/java/com/example/travel/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins("http://localhost:") // 프론트 주소
+                .allowedOrigins("http://localhost:8080") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
